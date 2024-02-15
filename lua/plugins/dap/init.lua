b1578eaa8b316933db222f33505856ead58a9719
Ray Zheng <raychenzheng@126.com>
2024-02-14 22:15:28 -0800

    changed dap ui layout

--- lua/plugins/dap/init.lua
+++ lua/plugins/dap/init.lua
@@ -13,17 +13,53 @@ if not dapui_python_status_ok then
   return
 end
 
-dapui.setup()
+local dap_vt_status_ok, dap_vt = pcall(require, "nvim-dap-virtual-text")
+if not dap_vt_status_ok then
+  return
+end
+
+dapui.setup({
+  layouts = { {
+    elements = { {
+        id = "scopes",
+        size = 0.25
+      }, {
+        id = "breakpoints",
+        size = 0.25
+      }, {
+        id = "stacks",
+        size = 0.25
+      }, {
+        id = "watches",
+        size = 0.25
+      } },
+    position = "left",
+    size = 0.2
+  }, {   -- want console on left side of repl
+    elements = { {
+        id = "console",
+        size = 0.5
+      }, {
+        id = "repl",
+        size = 0.5
+      } },
+    position = "bottom",
+    size = 0.4
+  } },
+})
+dap_vt.setup()
+
 dap_python.setup('~/mambaforge/bin/python')
+
 dap.listeners.before.attach.dapui_config = function()
   dapui.open()
 end
 dap.listeners.before.launch.dapui_config = function()
   dapui.open()
 end
-dap.listeners.before.event_terminated.dapui_config = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited.dapui_config = function()
-  dapui.close()
-end
+-- dap.listeners.before.event_terminated.dapui_config = function()
+--   dapui.close()
+-- end
+-- dap.listeners.before.event_exited.dapui_config = function()
+--   dapui.close()
+-- end
