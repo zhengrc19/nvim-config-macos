040424a25890c95fb719669be7d231f532d3d434
Ray Zheng <raychenzheng@126.com>
2024-02-16 10:00:04 -0800

    improved lsp config

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -23,38 +23,7 @@ mason_lsp.setup()
 
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
-local signs = {
-  { name = "DiagnosticSignError", text = "" },
-  { name = "DiagnosticSignWarn", text = "" },
-  { name = "DiagnosticSignHint", text = "" },
-  { name = "DiagnosticSignInfo", text = "" },
-}
-
-for _, sign in ipairs(signs) do
-  vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-end
-
-local config = {
-  severity_sort = true,
-  float = {
-    focusable = true,
-    style = "minimal",
-    border = "rounded",
-    source = "always",
-    header = "",
-    prefix = "",
-  },
-}
-
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = "rounded",
-})
-
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-  border = "rounded",
-})
-
-vim.diagnostic.config(config)
+require("plugins.lsp.options")
 
 mason_lsp.setup_handlers {
 	-- The first entry (without a key) will be the default handler
@@ -73,4 +42,4 @@ mason_lsp.setup_handlers {
 	end,
 }
 
-require("plugins.lsp.lsp_options")
+require("plugins.lsp.autocmds")
