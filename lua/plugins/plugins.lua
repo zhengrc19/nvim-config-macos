168f00331841f9aa99b1cd9a8c8d36a84ce6985a
Ray Zheng <raychenzheng@126.com>
2024-02-25 23:38:17 -0800

    added session manager

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -160,6 +160,16 @@ require("lazy").setup({
     config = true,
     keys = { -- load the plugin only when using it's keybinding:
       { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+    },
+  }, { -- session manager
+    'Shatur/neovim-session-manager',
+    dependencies = "nvim-lua/plenary.nvim",
+    config = function ()
+      local config = require('session_manager.config')
+      require('session_manager').setup({
+        autoload_mode = config.AutoloadMode.CurrentDir
+      })
+    end
   }
   -- TODOS
   -- nvim-spectre
