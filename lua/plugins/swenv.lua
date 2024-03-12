cfaba1bfe4490e65812838718fc7c06b0b2f03b0
Ray Zheng <raychenzheng@126.com>
2024-03-12 15:57:02 -0700

    merged swenv plugin settings into plugin code

--- lua/plugins/swenv.lua
+++ lua/plugins/swenv.lua
@@ -8,20 +8,20 @@ swenv.setup({
   -- Gets the argument `venvs_path` set below.
   -- By default just lists the entries in `venvs_path`.
   -- adds the base path, which is not in envs/ but in the root path.
-  get_venvs = function(venvs_path)
-    local venvs = require('swenv.api').get_venvs(venvs_path)
-    -- add base env in conda
-    local path = os.getenv("CONDA_EXE")
-    if path then
-      path = vim.fn.fnamemodify(path, ":p:h:h")
-      table.insert(venvs, 1, {
-        name = "base",
-        path = path,
-        source = "conda"
-      })
-    end
-    return venvs
-  end,
+  -- get_venvs = function(venvs_path)
+  --   local venvs = require('swenv.api').get_venvs(venvs_path)
+  --   -- add base env in conda
+  --   local path = os.getenv("CONDA_EXE")
+  --   if path then
+  --     path = vim.fn.fnamemodify(path, ":p:h:h")
+  --     table.insert(venvs, 1, {
+  --       name = "base",
+  --       path = path,
+  --       source = "conda"
+  --     })
+  --   end
+  --   return venvs
+  -- end,
   -- Something to do after setting an environment, for example call vim.cmd.LspRestart
   post_set_venv = function (venv)
     vim.cmd.LspRestart()
